bfa6dad0f0184cea983d2da920bc1e13a5705d77
Kavin Babu <86172980+Kbcoder69@users.noreply.github.com>
2023-03-18 23:59:06 +0500

    Changed the layout of the cards in the homepage

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -52,9 +52,13 @@ export const Home = () => {
         <div className="row">
           <div className="col-md-3 mb-3">
             <div className="card" style={{ width: '18rem' }}>
-              <img src="..." className="card-img-top" alt="..." />
+              <img src={require('../../../content/images/elementor-placeholder-image.png')} className="card-img-top" />
               <div className="card-body">
+                <h5 className="card-title">Card with stretched link</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary stretched-link">
+                  Go somewhere
+                </a>
               </div>
             </div>
           </div>
@@ -62,7 +66,11 @@ export const Home = () => {
             <div className="card" style={{ width: '18rem' }}>
               <img src="..." className="card-img-top" alt="..." />
               <div className="card-body">
+                <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
               </div>
             </div>
           </div>
@@ -70,7 +78,11 @@ export const Home = () => {
             <div className="card" style={{ width: '18rem' }}>
               <img src="..." className="card-img-top" alt="..." />
               <div className="card-body">
+                <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
               </div>
             </div>
           </div>
@@ -78,7 +90,11 @@ export const Home = () => {
             <div className="card" style={{ width: '18rem' }}>
               <img src="..." className="card-img-top" alt="..." />
               <div className="card-body">
+                <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
               </div>
             </div>
           </div>

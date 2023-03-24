f7c48fcb95b916f08afd4e3abdeb0d70deaacf00
Kavin Babu <86172980+Kbcoder69@users.noreply.github.com>
2023-03-24 23:13:54 +0500

    Added slides to the homepage

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -27,9 +27,48 @@ export const Home = () => {
 
   return (
     <main className="mt-5 pt-3">
-      <div className="container" style={{ position: 'relative' }}>
+      {/*<div className="container" style={{ position: 'relative' }}>
         <img src="content/images/student-pic.jpg" alt="logo" style={{ width: '100%' }} />
         <div className="welcomeText"> Welcome to Clubping</div>
+      </div>
+      */}
+      <div className="welcomeText" style={{ position: 'relative', textAlign: 'center' }}>
+        {' '}
+        Welcome to Clubping
+      </div>
+
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="content/images/elementor-placeholder-image.png" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="content/images/student-pic.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
       <div className="container">
         <div className="row">

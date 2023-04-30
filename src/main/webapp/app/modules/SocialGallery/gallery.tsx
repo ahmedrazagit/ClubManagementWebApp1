625bfb359846b103ab45411b99eb796a83bd4472
Ahmed Raza <axa1874@student.bham.ac.uk>
2023-05-01 00:36:58 +0400

    Added the social gallery feature, instagram API and some other frontend changes

--- src/main/webapp/app/modules/SocialGallery/gallery.tsx
+++ src/main/webapp/app/modules/SocialGallery/gallery.tsx
@@ -1,5 +1,83 @@
 import React, { useEffect, useState } from 'react';
-import Instafeed from 'app/modules/SocialGallery/instagramfeed';
+import './insta.css';
+import axios from 'axios';
+
+const Gallery = () => {
+  const [accessToken, setAccessToken] = useState<string>(
+    'IGQVJWREtkSmUzZAVJ2SGtncWUzXzJINDktSUVldGM2ZAmEtUk0yeFl1WEZApYkgwV2RqYXZAyVmRmMWZAEblNOdVRiS0FDLVIwa05NR3JRRGlZASXdUa1N1ZA1luRm9ydy1TeFR5QThjUTdOUGx6S1BrNEoycAZDZD'
+  );
+
+  const [media, setMedia] = useState([]);
+
+  const accounts = [
+    {
+      label: 'Select an account',
+      token:
+        'IGQVJWREtkSmUzZAVJ2SGtncWUzXzJINDktSUVldGM2ZAmEtUk0yeFl1WEZApYkgwV2RqYXZAyVmRmMWZAEblNOdVRiS0FDLVIwa05NR3JRRGlZASXdUa1N1ZA1luRm9ydy1TeFR5QThjUTdOUGx6S1BrNEoycAZDZD',
+    },
+    {
+      label: 'UOBD Dance Club',
+      token:
+        'IGQVJWR2lwazBtdm1EeTN6MEtJa2lNN1U2Q2NON1hHbkxlSUpzaGUxUGwxQ1ZAoR05VUno1dTQtNk5oS004ZA3ZAxUC04b1BBQ2NfVF92b3M4ejJtWU02Qi1ZAZAHlCZAnhHczItTTRpRnFYaUJMYmE4YUw4UQZDZD',
+    },
+    {
+      label: 'Account 3',
+      token: 'access_token_for_account_3',
+    },
+  ];
+
+  useEffect(() => {
+    if (accessToken) {
+      axios
+        .get(
+          `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${accessToken}`
+        )
+        .then(response => {
+          setMedia(response.data.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, [accessToken]);
+
+  return (
+    <div>
+      <div className="header">
+        <span className="banner">Social Gallery</span>
+        <a href="/featured" className="button">
+          Wanna Get Featured
+        </a>
+        <div className="menu">
+          <select className="account-select" onChange={e => setAccessToken(e.target.value)} value={accessToken}>
+            {accounts.map(({ label, token }) => (
+              <option key={token} value={token}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      {media.length > 0 && (
+        <div id="instafeed" className="image-grid">
+          {media.map(item => (
+            <div key={item.id} className="image-container">
+              <a href={item.permalink} target="_blank" rel="noopener noreferrer">
+                <img src={item.media_url} alt={item.caption} />
+              </a>
+              <div className="image-caption">{item.caption}</div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Gallery;
+
+/*import React, { useEffect, useState } from 'react';
+
 import './insta.css';
 
 const Gallery = () => {
@@ -59,4 +137,4 @@ const Gallery = () => {
   );
 };
 
-export default Gallery;
+export default Gallery;*/
